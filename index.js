6f7f2cbb6a15c37ef0c6fc88ee4cc690be5adf89
UC <drealuche@gmail.com>
2026-02-01 18:48:04 +0100

    I want to commit the following changes: Add a new function `newCard` in `index.js` that adds a new card with a value of 6 to the current sum and then calls `startGame()` to update the game

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardEl = document.getElementById("card-el");
 
 function startGame(){
+
+    cardEl.textContent = `Cards: ${firstCard} 💠 ${secondCard}`
+    sumEl.textContent = `Sum: ${sum}`;
+
     if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂"
     } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
+    message = "You've got Blackjack! 🥳"
     hasBlackJack = true
     } else {
     message = "You're out of the game! 😭"
     isAlive = false
     }
-    console.log(message)
-    }
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+    
+    messageEl.textContent = message;
+
+}
+
+function newCard() {
+   
+    let card = 6;
+    // 2. Add the new card to the sum variable
+    sum += card;
+    // 3. Call startGame()
+    startGame();
+}
+
 
 
