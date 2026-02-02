bce53226cf206293a305bc58b13bb472ffc7225a
UC <drealuche@gmail.com>
2026-02-02 10:38:41 +0100

    I made some changes to the blackjack game code to  implement a chip system. Players start with 20 chips,  and drawing a new card costs 5 chips. If the player gets blackjack,   they win 7 chips.

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = true; //for starting the game;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
-let cardEl = document.getElementById("card-el");
+let cardsEl = document.getElementById("card-el");
 
-function startGame(){
+let playerEl = document.getElementById("userDashboard");
 
-    cardEl.textContent = `Cards: ${firstCard} 💠 ${secondCard}`
-    sumEl.textContent = `Sum: ${sum}`;
 
-    if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-    } else if (sum === 21) {
-    message = "You've got Blackjack! 🥳"
-    hasBlackJack = true
+let player = {
+    name: "Uche",
+    chips: 0
+}
+
+// a function for returning a random card
+function getRandomCard() {
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumer = Math.floor( Math.random()*13 ) + 1;
+    if (randomNumer > 10) {
+        return 10;
+    } else if (randomNumer === 1) {
+        return 11;
     } else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+        return randomNumer;
+    }
+}
+
+
+function startGame() {
+    isAlive = true;
+    player.chips = 20;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+
+    renderGame();
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards:"
+    // a for loop that renders out all the cards instead of just two
+    for (i = 0; i < cards.length; i++){
+          cardsEl.textContent += cards[i] + " ";
+    }
+
+    sumEl.textContent = `Sum: ${sum}`;
+    playerEl.textContent = player.name + ": $" + player.chips;
+
+    if (sum <= 20 ) {
+    message = "Do you want to draw a new card? 🙂";
+    player.chips -= 5;
+    } else if (sum === 21) {
+    message = "You've got Blackjack! 🥳";
+    hasBlackJack = true;
+    player.chips += 7;
+    } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+
     }
     
     messageEl.textContent = message;
@@ -29,11 +71,12 @@ function startGame(){
 
 function newCard() {
    
-    let card = 6;
-    // 2. Add the new card to the sum variable
-    sum += card;
-    // 3. Call startGame()
-    startGame();
+   if(isAlive === true && hasBlackJack === false){
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
 }
 
 
